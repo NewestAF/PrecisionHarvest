8a7754625dbfabf73ec291cd307aacdfd41c2691
seongjookim <seongjookim@postech.ac.kr>
2025-12-29 18:41:02 +0900

    chore: add manager's instance in main class

--- src/main/kotlin/com/newestaf/precisionHarvest/PrecisionHarvest.kt
+++ src/main/kotlin/com/newestaf/precisionHarvest/PrecisionHarvest.kt
@@ -1,13 +1,18 @@
 package com.newestaf.precisionHarvest
 
 import com.newestaf.precisionHarvest.config.ConfigManager
+import com.newestaf.precisionHarvest.minigame.MinigameManager
+import com.newestaf.precisionHarvest.trigger.HarvestTrigger
 import org.bukkit.plugin.java.JavaPlugin
 
 class PrecisionHarvest : JavaPlugin() {
     val configManager: ConfigManager = ConfigManager(this)
+    val minigameManager: MinigameManager = MinigameManager(this, configManager)
 
     override fun onEnable() {
         configManager.loadConfig()
+
+        server.pluginManager.registerEvents(HarvestTrigger(configManager, minigameManager), this)
     }
 
     override fun onDisable() {
